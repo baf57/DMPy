44d441e61a1185620af2f7de82487b4f4b0ae2b4
JCEP Lab <jcepquantinium@gmail.com>
2023-07-27 16:23:56 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DMPy',
+    version='0.0.1',
+    url='https://github.com/baf57/DMPy.git',
+    author='Brayden Freitas',
+    author_email='andrewbrayden98@gmail.com',
+    description='Simple Python API for controlling the Thorlabs DMP40 deformable mirror',
+    packages=find_packages(),
+)
